182c8bccd7103e7753623cc44017b3f6f1956faa
Mike Qin <mikeyqin@gmail.com>
2025-07-30 21:24:20 -0700

    tried chainlit starters

--- 11-mcp/code_samples/github-mcp/app.py
+++ 11-mcp/code_samples/github-mcp/app.py
@@ -32,6 +32,32 @@ from azure.search.documents.indexes.models import SearchIndex, SimpleField, Sear
 
 # Load environment variables
 load_dotenv()
+# Load environment variables
+load_dotenv("/Users/mikeqin/code/ml_learning/ai-agents-for-beginners/.env.azure.ai.agent", override=True)
+@cl.set_starters
+async def set_starters():
+    return [
+        cl.Starter(
+            label="Morning routine ideation",
+            message="Can you help me create a personalized morning routine that would help increase my productivity throughout the day? Start by asking me about my current habits and what activities energize me in the morning.",
+            icon="/public/favicon.svg",
+            ),
+        cl.Starter(
+            label="Explain superconductors",
+            message="Explain superconductors like I'm five years old.",
+            icon="/public/favicon.svg",
+            ),
+        cl.Starter(
+            label="Python script for daily email reports",
+            message="Write a script to automate sending daily email reports in Python, and walk me through how I would set it up.",
+            icon="/public/favicon.svg",
+            ),
+        cl.Starter(
+            label="Text inviting friend to wedding",
+            message="Write a text asking a friend to be my plus-one at a wedding next month. I want to keep it super short and casual, and offer an out.",
+            icon="/public/favicon.svg",
+            )
+        ]
 
 # Configure logging
 logging.basicConfig(
